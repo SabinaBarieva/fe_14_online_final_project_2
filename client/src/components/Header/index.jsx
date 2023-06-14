68255e2432dcd26991003ea5242ed82d44e8c9d9
Vadim Zayats <vadyk.za@gmail.com>
2023-06-14 20:42:19 +0300

    router refactor

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,14 +1,9 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Footer from "../Footer";
 function Header() {
   return (
     <>
-      <header>
-        <NavLink to="/home">main content</NavLink>
-        <NavLink to="/product">product</NavLink>
-        <NavLink to="/categories">Categories and filters</NavLink>
-        <NavLink to="/basket">basket content</NavLink>
-      </header>
+      <header>header</header>
       <Outlet />
       <Footer />
     </>
--- client/src/pages/CatsAndFilters/index.jsx
+++ client/src/pages/CatsAndFilters/index.jsx
@@ -1,4 +1,13 @@
+import { NavLink } from "react-router-dom";
 const CatsAndFilters = () => {
-  return <>Categories And Filters Content</>;
+  return (
+    <>
+      <NavLink to="/home">main content</NavLink>
+      <NavLink to="/product">product</NavLink>
+      {/* <NavLink to="/categories">Categories and filters</NavLink> */}
+      <NavLink to="/basket">basket content</NavLink>
+      <div>Categories And Filters Content</div>{" "}
+    </>
+  );
 };
 export default CatsAndFilters;
