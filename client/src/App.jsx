0af3bfd75bff10a278e4e33f0d2d0cbd6c7a613b
Vadim Zayats <vadyk.za@gmail.com>
2023-06-14 17:21:15 +0300

    router+redux

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Provider } from "react-redux";
+import BasketContent from "./pages/Basket";
+import CatsAndFilters from "./pages/CatsAndFilters";
+import MainContent from "./pages/Main";
+import ProductContent from "./pages/Product";
+import Header from "./components/Header";
+import store from "./redux/store";
+
+function App() {
+  return (
+    <>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Header />}>
+              <Route path="home" element={<Navigate to="/" replace />}></Route>
+              <Route index element={<MainContent />}></Route>
+              <Route path="basket" element={<BasketContent />}></Route>
+              <Route path="categories" element={<CatsAndFilters />}></Route>
+              <Route path="product" element={<ProductContent />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
+  );
+}
+
+export default App;
