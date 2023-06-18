09ba305c7684e325a754be7147c6ef0c177644c0
Eugene Emets <eu.emets@gmail.com>
2023-06-18 09:41:50 +0300

    Add .env and keys_dev.js to gitignore and remove that files from future commiting

--- config/keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI:'mongodb+srv://oleksandrakorkh:mwPwEVHmnGXKTdWm@cluster0.svplfkl.mongodb.net/?retryWrites=true&w=majority',
-  secretOrKey: 'random very very secret string',
-};
