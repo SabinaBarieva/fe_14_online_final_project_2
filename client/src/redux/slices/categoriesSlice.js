508d46535d53120e9b34544ec366f6ff8cd9397d
Eugene Emets <eu.emets@gmail.com>
2023-06-19 12:11:38 +0300

    Add functions and reducers for fetching products, categories Add reducer for storing errors (for future show errors)

---
+++ client/src/redux/slices/categoriesSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+import getCategories from '../../api/getCategories';
+
+const initialState = {
+  isFetching: false,
+  isFetched: false,
+  categories: [],
+};
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    startFetchingCategories: (state) => {
+      state.isFetching = true;
+    },
+    finishFetchingCategories: (state, action) => {
+      state.isFetching = true;
+      state.isFetched = true;
+      state.categories = action.payload.categories;
+    },
+    errorFetchingCategories: (state) => {
+      state.isFetching = false;
+      state.isFetched = false;
+    },
+  },
+});
+
+export const { startFetchingCategories, finishFetchingCategories } =
+  categoriesSlice.actions;
+export default categoriesSlice.reducer;
+
+export const fetchCategories = () => async (dispatch) => {
+  dispatch(startFetchingCategories());
+  const categories = await getCategories();
+  dispatch(
+    finishFetchingCategories({
+      categories,
+    })
+  );
+};
