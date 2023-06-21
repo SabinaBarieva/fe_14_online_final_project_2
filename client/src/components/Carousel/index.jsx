b948c7e4b2afa93024c0669cb00f4bdae59cad0b
Kostiantyn Hrytsai <kos2@ukr.net>
2023-06-22 01:12:57 +0300

    working from Carousel

--- client/src/components/Carousel/index.jsx
+++ client/src/components/Carousel/index.jsx
@@ -3,18 +3,31 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import {connect} from 'react-redux';
 // import Product from '../Product';
-import getProducts from '../../api/getProducts';
+// import getProducts from '../../api/getProducts';
 
-const Carousel = ({product}) => {
-  const { id, name, currentPrice, categories, imageUrls, color, brand } = product;
+const Carousel = ({products}) => {
+  // const { id, name, currentPrice, categories, imageUrls, color, brand } = products;
   // console.log(product)
+  
   return (
+    <> 
+    <h2 className="title-products">Featured Products</h2>
     <Swiper spaceBetween={50} slidesPerView={3} onSlideChange={() => console.log('slide change')} onSwiper={(swiper) => console.log(swiper)}>
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      {products.map((product)=> (
+          <SwiperSlide
+          key={product.id}
+          product={product}
+          >
+          
+          </SwiperSlide>
+      ))}
     </Swiper>
+    </>
+   
   );
 };
-export default Carousel;
+const mapStateToProps = (state) => ({
+  products: state.products.products,
+});
+
+export default connect(mapStateToProps, null)(Carousel);
