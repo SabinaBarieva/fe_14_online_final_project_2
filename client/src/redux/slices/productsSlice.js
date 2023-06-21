4317e099ce8438b3fcbfe5fe3b810776b1f42183
Vadim Zayats <vadyk.za@gmail.com>
2023-06-21 15:01:02 +0300

    createAsyncThunk is working

--- client/src/redux/slices/productsSlice.js
+++ client/src/redux/slices/productsSlice.js
@@ -25,14 +25,50 @@ export default productsSlice.reducer;
 export const getProducts = createAsyncThunk(
   "products/getProducts",
   async (_, { dispatch, rejectWithValue }) => {
-    // const resnonse = await fetch("./collection.json");
-    // const data = await resnonse.json();
-    // dispatch(updateProducts(data));
-    dispatch(setProducts(["qwe"]))
+    const resnonse = await fetch("http://localhost:4000/api/products");
+    const data = await resnonse.json();
+    dispatch(setProducts(data))
   }
 );
 
 
+// import { createSlice } from '@reduxjs/toolkit';
+// import getProducts from '../../api/getProducts';
+// import { setErrorMessage } from './errorsSlice';
+
+// const initialState = {
+//   products: [],
+//   total: undefined,
+//   isFetching: false,
+//   isFetched: false,
+// };
+// // const getProducts = createAsyncThunk();
+// const productsSlice = createSlice({
+//   name: 'products',
+//   initialState,
+//   reducers: {
+//     startFetchingProducts: (state) => {
+//       state.isFetching = true;
+//     },
+//     finishFetchingProducts: (state, action) => {
+//       state.isFetching = false;
+//       state.isFetched = true;
+//       state.products = action.payload.products;
+//       state.total = action.payload.total;
+//     },
+//     errorFetchingProducts: (state, action) => {
+//       state.isFetching = false;
+//     },
+//   },
+// });
+// console.log(productsSlice);
+// export const {
+//   startFetchingProducts,
+//   finishFetchingProducts,
+//   errorFetchingProducts,
+// } = productsSlice.actions;
+// export default productsSlice.reducer;
+// export const fetchProducts =
 //   ({ categories = [], perPage = 10, startPage = 1 }) =>
 //   async (dispatch) => {
 //     dispatch(startFetchingProducts());
