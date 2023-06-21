36be4ee2422d2b8ce0a7572692aba7f32060657e
Vadim Zayats <vadyk.za@gmail.com>
2023-06-21 14:44:55 +0300

    productSlite fetching in process

--- client/src/redux/slices/productsSlice.js
+++ client/src/redux/slices/productsSlice.js
@@ -1,65 +1,62 @@
-import { createSlice } from '@reduxjs/toolkit';
-import getProducts from '../../api/getProducts';
-import { setErrorMessage } from './errorsSlice';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import getProducts from '../../api/getProducts';
+// import { setErrorMessage } from './errorsSlice';
 
 const initialState = {
   products: [],
-  total: undefined,
-  isFetching: false,
-  isFetched: false,
 };
-// const getProducts = createAsyncThunk();
+
 const productsSlice = createSlice({
   name: 'products',
   initialState,
   reducers: {
-    startFetchingProducts: (state) => {
-      state.isFetching = true;
-    },
-    finishFetchingProducts: (state, action) => {
-      state.isFetching = false;
-      state.isFetched = true;
-      state.products = action.payload.products;
-      state.total = action.payload.total;
-    },
-    errorFetchingProducts: (state, action) => {
-      state.isFetching = false;
+    setProducts: (state, action) => {
+      state.products = action.payload;
     },
   },
 });
-console.log(productsSlice);
+
 export const {
-  startFetchingProducts,
-  finishFetchingProducts,
-  errorFetchingProducts,
+  updateProducts
 } = productsSlice.actions;
+
 export default productsSlice.reducer;
-export const fetchProducts =
-  ({ categories = [], perPage = 10, startPage = 1 }) =>
-  async (dispatch) => {
-    dispatch(startFetchingProducts());
-    try {
-      const result = await getProducts({
-        categories,
-        startPage,
-        perPage,
-      });
-      dispatch(
-        finishFetchingProducts({
-          products: result.products,
-          total: result.productsQuantity,
-        })
-      );
-    } catch (e) {
-      dispatch(
-        errorFetchingProducts({
-          error: e.message,
-        })
-      );
-      dispatch(
-        setErrorMessage({
-          error: e.message,
-        })
-      );
-    }
-  };
+
+export const getProducts = createAsyncThunk(
+  "products/getProducts",
+  async (_, { dispatch, rejectWithValue }) => {
+    const resnonse = await fetch("./collection.json");
+    const data = await resnonse.json();
+    dispatch(updateProducts(data));
+  }
+);
+
+
+//   ({ categories = [], perPage = 10, startPage = 1 }) =>
+//   async (dispatch) => {
+//     dispatch(startFetchingProducts());
+//     try {
+//       const result = await getProducts({
+//         categories,
+//         startPage,
+//         perPage,
+//       });
+//       dispatch(
+//         finishFetchingProducts({
+//           products: result.products,
+//           total: result.productsQuantity,
+//         })
+//       );
+//     } catch (e) {
+//       dispatch(
+//         errorFetchingProducts({
+//           error: e.message,
+//         })
+//       );
+//       dispatch(
+//         setErrorMessage({
+//           error: e.message,
+//         })
+//       );
+//     }
+//   };
