085704c28a19b697d47481e26284f0d87f105cde
Vadim Zayats <vadyk.za@gmail.com>
2023-06-22 17:31:29 +0300

    Current Product component + slice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,21 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
-import testSlice from './slices/testSlice';
 import productsSlice from './slices/productsSlice';
 import categoriesSlice from './slices/categoriesSlice';
 import errorsSlice from './slices/errorsSlice';
 import sliceModal from './slices/modalSlice';
 import sliceForm from './slices/formSlice';
-import currentProductSlice from './slices/currentProductSlice';
+import productSlice from './slices/productSlice';
 
 const store = configureStore({
   reducer: {
-    test: testSlice,
     products: productsSlice,
     categories: categoriesSlice,
     errors: errorsSlice,
     toolkitModal: sliceModal,
     toolkitForm: sliceForm,
-    currentProduct: currentProductSlice,
+    product: productSlice
   },
 });
 
