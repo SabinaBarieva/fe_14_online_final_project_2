2583bf17601e2c7400df58927164c459e9a41824
Vadim Zayats <vadyk.za@gmail.com>
2023-06-22 16:55:47 +0300

    productFetch started

--- client/src/pages/Main/index.jsx
+++ client/src/pages/Main/index.jsx
@@ -1,17 +1,6 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
 import ProductDescription from '../../components/ProductDescription'
-import { product } from '../../redux/selectors';
-import { getProduct } from '../../redux/slices/productSlice';
 
 const MainContent = () => {
-  const dispatch = useDispatch()
-  const currentProduct = useSelector(product)
-
-  useEffect(()=>{
-  dispatch(getProduct())
-  }, [dispatch])
-
   return (
     <>
       <div>Main Content</div>
