057eca4844a691220ace782d2e461b5255a07491
Vadim Zayats <vadyk.za@gmail.com>
2023-06-22 12:20:45 +0300

    productFetch started

--- client/src/redux/slices/productsSlice.js
+++ client/src/redux/slices/productsSlice.js
@@ -1,100 +1,99 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// import getProducts from '../../api/getProducts';
-// import { setErrorMessage } from './errorsSlice';
+import { createSlice } from '@reduxjs/toolkit';
+import getProducts from '../../api/getProducts';
+import { setErrorMessage } from './errorsSlice';
 
 const initialState = {
   products: [],
+  total: undefined,
+  isFetching: false,
+  isFetched: false,
 };
-
+// const getProducts = createAsyncThunk();
 const productsSlice = createSlice({
   name: 'products',
   initialState,
   reducers: {
-    setProducts: (state, action) => {
-      state.products = action.payload;
-
+    startFetchingProducts: (state) => {
+      state.isFetching = true;
+    },
+    finishFetchingProducts: (state, action) => {
+      state.isFetching = false;
+      state.isFetched = true;
+      state.products = action.payload.products;
+      state.total = action.payload.total;
+    },
+    errorFetchingProducts: (state, action) => {
+      state.isFetching = false;
     },
   },
 });
-
+console.log(productsSlice);
 export const {
-  setProducts
+  startFetchingProducts,
+  finishFetchingProducts,
+  errorFetchingProducts,
 } = productsSlice.actions;
-
 export default productsSlice.reducer;
+export const fetchProducts =
+  ({ categories = [], perPage = 10, startPage = 1 }) =>
+  async (dispatch) => {
+    dispatch(startFetchingProducts());
+    try {
+      const result = await getProducts({
+        categories,
+        startPage,
+        perPage,
+      });
+      dispatch(
+        finishFetchingProducts({
+          products: result.products,
+          total: result.productsQuantity,
+        })
+      );
+    } catch (e) {
+      dispatch(
+        errorFetchingProducts({
+          error: e.message,
+        })
+      );
+      dispatch(
+        setErrorMessage({
+          error: e.message,
+        })
+      );
+    }
+  };
 
-export const getProducts = createAsyncThunk(
-  "products/getProducts",
-  async (_, { dispatch, rejectWithValue }) => {
-    const resnonse = await fetch("/api/products");
-    const data = await resnonse.json();
-    dispatch(setProducts(data))
-  }
-);
 
-
-// import { createSlice } from '@reduxjs/toolkit';
-// import getProducts from '../../api/getProducts';
-// import { setErrorMessage } from './errorsSlice';
+// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 // const initialState = {
 //   products: [],
-//   total: undefined,
-//   isFetching: false,
-//   isFetched: false,
 // };
-// // const getProducts = createAsyncThunk();
+
 // const productsSlice = createSlice({
 //   name: 'products',
 //   initialState,
 //   reducers: {
-//     startFetchingProducts: (state) => {
-//       state.isFetching = true;
-//     },
-//     finishFetchingProducts: (state, action) => {
-//       state.isFetching = false;
-//       state.isFetched = true;
-//       state.products = action.payload.products;
-//       state.total = action.payload.total;
-//     },
-//     errorFetchingProducts: (state, action) => {
-//       state.isFetching = false;
+//     setProducts: (state, action) => {
+//       state.products = action.payload;
+
 //     },
 //   },
 // });
-// console.log(productsSlice);
+
 // export const {
-//   startFetchingProducts,
-//   finishFetchingProducts,
-//   errorFetchingProducts,
+//   setProducts
 // } = productsSlice.actions;
+
 // export default productsSlice.reducer;
-// export const fetchProducts =
-//   ({ categories = [], perPage = 10, startPage = 1 }) =>
-//   async (dispatch) => {
-//     dispatch(startFetchingProducts());
-//     try {
-//       const result = await getProducts({
-//         categories,
-//         startPage,
-//         perPage,
-//       });
-//       dispatch(
-//         finishFetchingProducts({
-//           products: result.products,
-//           total: result.productsQuantity,
-//         })
-//       );
-//     } catch (e) {
-//       dispatch(
-//         errorFetchingProducts({
-//           error: e.message,
-//         })
-//       );
-//       dispatch(
-//         setErrorMessage({
-//           error: e.message,
-//         })
-//       );
-//     }
-//   };
+
+// export const getProducts = createAsyncThunk(
+//   "products/getProducts",
+//   async (_, { dispatch, rejectWithValue }) => {
+//     const resnonse = await fetch("/api/products");
+//     const data = await resnonse.json();
+//     dispatch(setProducts(data))
+//   }
+// );
+
