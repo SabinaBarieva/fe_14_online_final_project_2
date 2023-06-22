3f32e483a78a602d9843668bd3e69f01a7eafa9f
Vadim Zayats <vadyk.za@gmail.com>
2023-06-22 16:59:49 +0300

    productFetch started

--- client/src/pages/Main/index.jsx
+++ client/src/pages/Main/index.jsx
@@ -1,12 +1,7 @@
-import ProductDescription from '../../components/ProductDescription'
-
 const MainContent = () => {
   return (
     <>
       <div>Main Content</div>
-      <br />
-
-    <ProductDescription/>
     </>
   );
 };
