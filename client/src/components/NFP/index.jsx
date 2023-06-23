7ff5c45b3be6ee9c440aaf950037408843201fc5
Vadim Zayats <vadyk.za@gmail.com>
2023-06-23 22:41:19 +0300

    404 page added

--- client/src/components/NFP/index.jsx
+++ client/src/components/NFP/index.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { styled } from '@mui/system';
+import { Button } from '@mui/material';
+import Grid from '@mui/material/Grid';
 
 function PageNotFound() {
-  return <div>PAGE NOT FOUNDED</div>;
+  const Code = styled('div')({
+    textAlign: 'right',
+    letterSpacing: '0.015em',
+    color: '#9A9292',
+  });
+  return (
+    <Grid container sx={{ width: '85%', margin: '0 auto' }}>
+      Oops, the third party is not clear You have entered a wrong address, or
+      else such a side on the site is no longer available.
+      <Link to="/home">back to the home page</Link>
+    </Grid>
+  );
 }
 
 export default PageNotFound;
