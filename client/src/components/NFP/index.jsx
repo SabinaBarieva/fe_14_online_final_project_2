1a3ce389a0191babbeaa9a695dfa199f2f08f885
Vadim Zayats <vadyk.za@gmail.com>
2023-06-23 22:47:22 +0300

    404 page edited

--- client/src/components/NFP/index.jsx
+++ client/src/components/NFP/index.jsx
@@ -5,16 +5,48 @@ import { Button } from '@mui/material';
 import Grid from '@mui/material/Grid';
 
 function PageNotFound() {
-  const Code = styled('div')({
-    textAlign: 'right',
-    letterSpacing: '0.015em',
+  const Text = styled('div')({
+    textAlign: 'center',
     color: '#9A9292',
   });
   return (
     <Grid container sx={{ width: '85%', margin: '0 auto' }}>
-      Oops, the third party is not clear You have entered a wrong address, or
-      else such a side on the site is no longer available.
-      <Link to="/home">back to the home page</Link>
+      <Grid item md={12}>
+        <Text>
+          Oops, the third party is not clear
+          <br />
+          You have entered a wrong address, or else such a side on the site is
+          no longer available.
+        </Text>
+      </Grid>
+      <Grid
+        item
+        md={12}
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Link to="/home">
+          <Button
+            sx={{
+              marginTop: { xs: '10px', md: '0' },
+              padding: '9px 18px',
+              backgroundColor: '#211F1C',
+              color: '#FFF',
+              borderRadius: 0,
+              border: '1px solid #211F1C',
+              '&:hover': {
+                backgroundColor: '#FFF',
+                color: '#211F1C',
+                border: '1px solid #211F1C',
+              },
+            }}
+            variant="contained">
+            back to the home page
+          </Button>
+        </Link>
+      </Grid>
     </Grid>
   );
 }
