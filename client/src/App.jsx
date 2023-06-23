348efb1ff993231bf6b667bedc8dd301766e2978
Vadim Zayats <vadyk.za@gmail.com>
2023-06-23 07:15:37 +0300

    alteration to eslint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,24 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Provider } from "react-redux";
-import BasketContent from "./pages/Basket";
-import CatsAndFilters from "./pages/CatsAndFilters";
-import MainContent from "./pages/Main";
-import ProductContent from "./pages/Product";
-import Header from "./components/Header";
-import store from "./redux/store";
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Basket from './pages/Basket';
+import MainContent from './pages/Main';
+import Header from './components/Header';
+import store from './redux/store';
 
 function App() {
   return (
-    <>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Header />}>
-              <Route path="home" element={<Navigate to="/" replace />}></Route>
-              <Route index element={<MainContent />}></Route>
-              <Route path="basket" element={<BasketContent />}></Route>
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </Provider>
-    </>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route path="home" element={<Navigate to="/" replace />} />
+            <Route index element={<MainContent />} />
+            <Route path="basket" element={<Basket />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
