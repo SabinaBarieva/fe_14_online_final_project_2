25f41f6ce4d05443dd82b3ff05f5c3ac522a1a65
Vadim Zayats <vadyk.za@gmail.com>
2023-06-24 18:32:10 +0300

    pics do not uploaded

--- client/src/components/ProductDescription/index.jsx
+++ client/src/components/ProductDescription/index.jsx
@@ -52,7 +52,11 @@ const Guarantee = styled('div')({
 function ProductDescription() {
   const dispatch = useDispatch();
 
-  const {
+  useEffect(() => {
+    dispatch(getProduct(77552));
+  }, [dispatch]);
+
+  let {
     // enabled,
     // quantity,
     // categories,
@@ -67,90 +71,93 @@ function ProductDescription() {
     guarantee,
   } = useSelector(productCurrent);
 
-  useEffect(() => {
-    dispatch(getProduct(77552));
-  }, [dispatch]);
-
-  return (
-    <Grid container sx={{ width: '90%', margin: '15px auto' }}>
-      <Grid
-        item
-        xs={12}
-        md={5}
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <img
-          src={imageUrls}
-          alt={name}
-          width={{ xs: '323px', sm: '596px', md: '539px' }}
-          height={{ xs: '222px', sm: '355px', md: '660px' }}
-        />
-      </Grid>
-      <Grid
-        item
-        xs={12}
-        md={7}
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-around',
-        }}>
-        <Code>{itemNo} :CODE</Code>
-        <Title>
-          {brand} {name} {storage} {color}
-        </Title>
-        <Description>{description}</Description>
-        <Price>Price: {currentPrice}$</Price>
-        <Guarantee>Apple guarantee {guarantee}</Guarantee>
-        <Grid container>
-          <Grid item md={4} xs={12} sx={{ display: 'flex', gap: '14px' }}>
-            <CountBoxes
-              sx={{
-                width: { xs: '35px', sm: '57px', md: '46px' },
-                height: { xs: '35px', sm: '57px', md: '46px' },
-              }}>
-              -
-            </CountBoxes>
-            <CountBoxes
-              sx={{
-                width: { xs: '35px', sm: '57px', md: '46px' },
-                height: { xs: '35px', sm: '57px', md: '46px' },
-              }}>
-              1
-            </CountBoxes>
-            <CountBoxes
-              sx={{
-                width: { xs: '35px', sm: '57px', md: '46px' },
-                height: { xs: '35px', sm: '57px', md: '46px' },
-              }}>
-              +
-            </CountBoxes>
-          </Grid>
-          <Grid item md={8} xs={12}>
-            <Button
-              sx={{
-                marginTop: { xs: '10px', md: '0' },
-                padding: '9px 18px',
-                backgroundColor: { xs: '#F5F7FB', md: '#211F1C' },
-                color: { xs: '#616467', md: '#fff' },
-                borderRadius: 0,
-                border: '1px solid #211F1C',
-                '&:hover': {
-                  backgroundColor: { xs: '#211F1C', md: '#fff' },
-                  color: { xs: '#F5F7FB', md: '#211F1C' },
-                  border: { xs: '1px solid #211F1C', md: '1px solid #211F1C' },
-                },
-              }}
-              variant="contained">
-              Add to basket
-            </Button>
+  if (imageUrls) {
+    console.log(`${imageUrls[0]}`);
+    return (
+      <Grid container sx={{ width: '90%', margin: '15px auto' }}>
+        <Grid
+          item
+          xs={12}
+          md={5}
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <img
+            src={`${imageUrls[0]}`}
+            alt={name}
+            width={{ xs: '323px', sm: '596px', md: '539px' }}
+            height={{ xs: '222px', sm: '355px', md: '660px' }}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={7}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-around',
+          }}>
+          <Code>{itemNo} :CODE</Code>
+          <Title>
+            {brand} {name} {storage} {color}
+          </Title>
+          <Description>{description}</Description>
+          <Price>Price: {currentPrice}$</Price>
+          <Guarantee>Apple guarantee {guarantee}</Guarantee>
+          <Grid container>
+            <Grid item md={4} xs={12} sx={{ display: 'flex', gap: '14px' }}>
+              <CountBoxes
+                sx={{
+                  width: { xs: '35px', sm: '57px', md: '46px' },
+                  height: { xs: '35px', sm: '57px', md: '46px' },
+                }}>
+                -
+              </CountBoxes>
+              <CountBoxes
+                sx={{
+                  width: { xs: '35px', sm: '57px', md: '46px' },
+                  height: { xs: '35px', sm: '57px', md: '46px' },
+                }}>
+                1
+              </CountBoxes>
+              <CountBoxes
+                sx={{
+                  width: { xs: '35px', sm: '57px', md: '46px' },
+                  height: { xs: '35px', sm: '57px', md: '46px' },
+                }}>
+                +
+              </CountBoxes>
+            </Grid>
+            <Grid item md={8} xs={12}>
+              <Button
+                sx={{
+                  marginTop: { xs: '10px', md: '0' },
+                  padding: '9px 18px',
+                  backgroundColor: { xs: '#F5F7FB', md: '#211F1C' },
+                  color: { xs: '#616467', md: '#fff' },
+                  borderRadius: 0,
+                  border: '1px solid #211F1C',
+                  '&:hover': {
+                    backgroundColor: { xs: '#211F1C', md: '#fff' },
+                    color: { xs: '#F5F7FB', md: '#211F1C' },
+                    border: {
+                      xs: '1px solid #211F1C',
+                      md: '1px solid #211F1C',
+                    },
+                  },
+                }}
+                variant="contained">
+                Add to basket
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
-  );
+    );
+  }
+  return <div>LOADING</div>;
 }
 export default ProductDescription;
