fea795e12ee9159878c05df814039d5dfcf97a59
Eugene Emets <eu.emets@gmail.com>
2023-06-25 16:44:51 +0300

    fix some common format problems in files

--- client/src/redux/slices/productsSlice.js
+++ client/src/redux/slices/productsSlice.js
@@ -22,12 +22,11 @@ const productsSlice = createSlice({
       state.products = action.payload.products;
       state.total = action.payload.total;
     },
-    errorFetchingProducts: (state, action) => {
+    errorFetchingProducts: (state) => {
       state.isFetching = false;
     },
   },
 });
-console.log(productsSlice);
 export const {
   startFetchingProducts,
   finishFetchingProducts,
