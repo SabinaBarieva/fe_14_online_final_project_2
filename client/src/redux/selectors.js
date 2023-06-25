b470fecdc38693ececedc2092b6d65ae09c1428e
Vadim Zayats <vadyk.za@gmail.com>
2023-06-25 20:50:12 +0300

    one selector deleted

--- client/src/redux/selectors.js
+++ client/src/redux/selectors.js
@@ -1,3 +1,2 @@
 export const currentProduct = (state) => state.product.product;
-export const cpIsLoading = (state) => state.product.isLoading;
 export const cpIsLoaded = (state) => state.product.isLoaded;
