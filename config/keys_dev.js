9d04d97aff64d05aeec8a19dba788f57129c2f92
Vadim Zayats <vadyk.za@gmail.com>
2023-06-26 22:59:26 +0300

    eslint removed

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://oleksandrakorkh:mwPwEVHmnGXKTdWm@cluster0.svplfkl.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://oleksandrakorkh:QnR2So9Qczfhzl7Y@cluster0.svplfkl.mongodb.net/?retryWrites=true&w=majority",
   secretOrKey: "random very very secret string",
 };
