78e3bcf390db14600ea6cd5bccd3696359419b6d
Vadim Zayats <vadyk.za@gmail.com>
2023-06-26 16:01:11 +0300

    404 page added

--- client/src/components/NFP/index.jsx
+++ client/src/components/NFP/index.jsx
@@ -4,16 +4,27 @@ import { styled } from '@mui/system';
 import { Button } from '@mui/material';
 import Grid from '@mui/material/Grid';
 
+const Text = styled('div')({
+  color: '#9A9292',
+  margin: '20px 0',
+});
+
 function PageNotFound() {
-  const Text = styled('div')({
-    textAlign: 'center',
-    color: '#9A9292',
-  });
   return (
-    <Grid container sx={{ width: '85%', margin: '0 auto' }}>
-      <Grid item md={12}>
+    <Grid container sx={{ width: '85%', margin: '20px auto' }}>
+      <Grid
+        item
+        md={12}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
+        }}>
+        <img src="../../img/404error.png" alt="error" height={'400px'} />
         <Text>
-          Oops, the third party is not clear...
+          Oops, something went wrong...
           <br />
           You have entered a wrong address, or this page on the site is no
           longer available.
