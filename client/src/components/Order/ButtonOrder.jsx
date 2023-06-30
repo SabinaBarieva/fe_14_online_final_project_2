636b60e8a3f11dd4a7d5cd8aef7cf1935eb104fc
Olena Hodyna <crimeaodessa2006@gmail.com>
2023-06-30 21:12:31 +0200

    styles in order component changed

--- client/src/components/Order/ButtonOrder.jsx
+++ client/src/components/Order/ButtonOrder.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Container, ThemeProvider } from '@mui/material';
+import { Container } from '@mui/material';
 import OrderForm from './orderForm';
 import ModalOrdered from './modal';
 import { openForm } from '../../redux/slices/formSlice';
-import theme, { StyledButton } from '../../themes/themeOrder';
+import { StyledButton } from '../../themes/themeOrder';
 
 export default function ButtonOrder() {
   const dispatch = useDispatch();
@@ -21,22 +21,20 @@ export default function ButtonOrder() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <>
-        {buttons && (
-          <Container>
-            <StyledButton
-              variant="contained"
-              color="primary"
-              style={{ borderRadius: 0, textTransform: 'none' }}
-              onClick={showForm}>
-              Check Out
-            </StyledButton>
-          </Container>
-        )}
-        {form()}
-        {modal()}
-      </>
-    </ThemeProvider>
+    <>
+      {buttons && (
+        <Container>
+          <StyledButton
+            variant="contained"
+            color="primary"
+            style={{ borderRadius: 0, textTransform: 'none' }}
+            onClick={showForm}>
+            Check Out
+          </StyledButton>
+        </Container>
+      )}
+      {form()}
+      {modal()}
+    </>
   );
 }
--- client/src/themes/themeOrder.js
+++ client/src/themes/themeOrder.js
@@ -1,19 +1,23 @@
 import {
   Button,
-  createTheme,
   FormControl,
-  TextField,
   IconButton,
-  OutlinedInput,
+  InputBase,
+  Grid,
+  Typography,
+  Modal,
+  Container,
 } from '@mui/material';
 import { styled } from '@mui/system';
 
 const StyledForm = styled(FormControl)({
   gap: 5,
+  width: '100%',
 });
 
 const StyledButton = styled(Button)({
   '&.MuiButton-root': {
+    display: 'block',
     fontFamily: 'Lato',
     fontSize: '1.2rem',
     fontWeight: 400,
@@ -22,6 +26,7 @@ const StyledButton = styled(Button)({
     height: 50,
     margin: '10px auto',
     padding: '13px 18px',
+    backgroundColor: '#211F1C',
   },
   '&.MuiButton-contained': {
     borderRadius: 50,
@@ -35,14 +40,17 @@ const StyledIconButton = styled(IconButton)({
   borderRadius: '20%',
 });
 
-const StyledInputBase = styled(TextField)({
+const StyledInputBase = styled(InputBase)({
   margin: 5,
   width: '90%',
   boxSizing: 'border-box',
-  border: 'none',
-  '&.MuiInputBase-input': {
-    padding: '5!important',
-    '&::placeholder': {
+  border: 'black 1px solid',
+  borderRadius: 50,
+  minHeight: 40,
+  '& .MuiInputBase-input': {
+    padding: '10px 15px!important',
+    borderRadius: 50,
+    '& ::placeholder': {
       opacity: 0.5,
     },
   },
@@ -53,14 +61,69 @@ const StyledInputBase = styled(TextField)({
   },
 });
 
-const StyledOutlinedInput = styled(OutlinedInput)({
-  '& .MuiOutlinedInput-root': {
+const StyledGrid = styled(Grid)({
+  margin: 0,
+  maxWidth: 250,
+  paddingLeft: 10,
+  borderRadius: 20,
+  minWidth: 50,
+});
+
+const StyledTypography = styled(Typography)({
+  textAlign: 'center',
+  '&.MuiTypography-paragraph': {
+    margin: 0,
+    fontSize: '0.6rem',
     color: 'red',
-    backgroundColor: 'green',
-    outline: 'none',
-    minHeight: 40,
-    borderRadius: 50,
+    height: 12,
   },
+
+  '&.MuiTypography-h6': {
+    margin: '10px auto',
+  },
+
+  '&.MuiTypography-h5': {
+    margin: '10px auto',
+    color: '#334756',
+    fontFamily: 'Lato',
+    fontSize: '2rem',
+    fontWeight: 700,
+    lineHeight: '132%',
+    letterSpacing: '0.015rem',
+    textTransform: 'uppercase',
+  },
+});
+
+const StyledModal = styled(Modal)({
+  margin: 'auto',
+  height: '35%',
+  minWidth: 300,
+  width: '50%',
+  maxWidth: 500,
+  '& .MuiModal-backdrop': {
+    backgroundColor: 'white',
+    zIndex: -1,
+  },
+});
+
+const StyledModalContainer = styled(Container)({
+  padding: 20,
+  lineHeight: 1.5,
+  borderRadius: 20,
+  boxShadow: '0 0 30px 6px #42445a',
+  textAlign: 'center',
+});
+
+const StyledFormBackground = styled(Container)({
+  backgroundColor: 'white',
+  position: 'fixed',
+  zIndex: 0,
+  right: 0,
+  bottom: 0,
+  top: 0,
+  left: 0,
+  overflow: 'scroll',
+  height: '100%',
 });
 
 export {
@@ -68,90 +131,9 @@ export {
   StyledButton,
   StyledInputBase,
   StyledIconButton,
-  StyledOutlinedInput,
+  StyledGrid,
+  StyledTypography,
+  StyledModal,
+  StyledModalContainer,
+  StyledFormBackground,
 };
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#211F1C',
-    },
-    secondary: {
-      main: '#C4C4C4',
-    },
-  },
-  components: {
-    MuiModal: {
-      styleOverrides: {
-        root: {
-          zIndex: 'auto',
-          margin: 'auto',
-          position: 'relative!important',
-          height: '35%',
-          minWidth: 280,
-          width: '50%',
-          maxWidth: 500,
-          padding: 10,
-          lineHeight: 1.5,
-          borderRadius: 20,
-          boxShadow: '0 0 30px 6px #42445a',
-          textAlign: 'center',
-        },
-        backdrop: {
-          backgroundColor: 'white',
-          borderRadius: 20,
-        },
-      },
-    },
-    MuiTypography: {
-      styleOverrides: {
-        root: {
-          textAlign: 'center',
-        },
-        paragraph: {
-          margin: 0,
-          fontSize: '0.6rem',
-          color: 'red',
-          height: 12,
-        },
-        h6: {
-          margin: '10px auto',
-        },
-        h5: {
-          margin: '10px auto',
-          color: '#334756',
-          fontFamily: 'Lato',
-          fontSize: '2rem',
-          fontWeight: 700,
-          lineHeight: '132%',
-          letterSpacing: '0.015em',
-          textTransform: 'uppercase',
-        },
-      },
-    },
-    // MuiOutlinedInput: {
-    //   styleOverrides: {
-    //     root: {
-    //       color: 'red',
-    //       backgroundColor: 'green',
-    //       outline: 'none',
-    //       minHeight: 40,
-    //       borderRadius: 50,
-    //     },
-    //   },
-    // },
-    MuiGrid: {
-      styleOverrides: {
-        root: {
-          margin: 0,
-          maxWidth: 250,
-          paddingLeft: 10,
-          borderRadius: 20,
-          minWidth: 50,
-        },
-      },
-    },
-  },
-});
-
-export default theme;
