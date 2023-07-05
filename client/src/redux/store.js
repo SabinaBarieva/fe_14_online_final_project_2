35cb04eca222840adfa03a17478c941020d44fe3
SabinaBarieva <136485784+SabinaBarieva@users.noreply.github.com>
2023-07-05 14:50:10 +0300

    Reduxer for basket (#41)

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -6,6 +6,7 @@ import sliceModal from './slices/modalSlice';
 import sliceForm from './slices/formSlice';
 // eslint-disable-next-line import/no-named-as-default
 import productSlice from './slices/productSlice';
+import basketSlice from './slices/basketSlice';
 
 const store = configureStore({
   reducer: {
@@ -15,6 +16,7 @@ const store = configureStore({
     toolkitModal: sliceModal,
     toolkitForm: sliceForm,
     product: productSlice,
+    basket: basketSlice,
   },
 });
 
