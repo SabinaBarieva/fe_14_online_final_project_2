df0b2858e8d68fcbab0c7882b3080cf9fe13b83e
Vadim Zayats <vadyk.za@gmail.com>
2023-07-08 15:39:57 +0300

    basket functions in component ready

--- client/src/redux/slices/basketSlice.js
+++ client/src/redux/slices/basketSlice.js
@@ -49,23 +49,33 @@ const basketSlice = createSlice({
       state.priceAll = [];
     },
     addSeveraltoBasket: (state, action) => {
-      // const seachItem = state.itemsBasket.find(
-      //   (item) => item.itemNo === action.payload.itemNo
-      // );
-      // if (seachItem) {
-      // if (seachItem.count !== seachItem.quantity) {
-      //   seachItem.count += 1;
-      // } else {
-      //   window.confirm(
-      //     'Sorry, the product you have chosen is no longer in stock.'
-      //   );
-      // }
-      // } else {
-      state.itemsBasket.push({
-        ...action.payload,
-        count: action.payload.count,
-      });
-      // }
+      const seachItem = state.itemsBasket.find(
+        (item) => item.itemNo === action.payload.itemNo
+      );
+      if (seachItem) {
+        if (
+          action.payload.count + seachItem.count > seachItem.quantity &&
+          seachItem.quantity - seachItem.count !== 0
+        ) {
+          alert(
+            `ДЕМО СТРОКА! можна додати ще не більше ніж ${
+              seachItem.quantity - seachItem.count
+            } до вже вибраної кількості товарів`
+          );
+        }
+        if (seachItem.count !== seachItem.quantity) {
+          seachItem.count += action.payload.count;
+        } else {
+          window.confirm(
+            'Sorry, the product you have chosen is no longer in stock.'
+          );
+        }
+      } else {
+        state.itemsBasket.push({
+          ...action.payload,
+          count: action.payload.count,
+        });
+      }
       state.priceAll = state.itemsBasket.reduce(
         (sum, item) => item.currentPrice * item.count + sum,
         0
