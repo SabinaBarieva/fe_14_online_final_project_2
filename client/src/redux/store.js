35a208676c61e0db892affbaaccf31627fec9a94
Vadym <59732160+vadym-zaiats@users.noreply.github.com>
2023-07-09 12:50:30 +0300

    Vadym/ref (#53)
    * redux persist added
    
    * redux persist ok
    
    * redux persist whitelist added

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import productsSlice from './slices/productsSlice';
 import categoriesSlice from './slices/categoriesSlice';
 import errorsSlice from './slices/errorsSlice';
@@ -9,17 +20,33 @@ import productSlice from './slices/productSlice';
 import filtersSlice from './slices/filtersSlice';
 import basketSlice from './slices/basketSlice';
 
-const store = configureStore({
-  reducer: {
-    products: productsSlice,
-    categories: categoriesSlice,
-    errors: errorsSlice,
-    toolkitModal: sliceModal,
-    toolkitForm: sliceForm,
-    product: productSlice,
-    filters: filtersSlice,
-    basket: basketSlice,
-  },
+const rootReducer = combineReducers({
+  products: productsSlice,
+  categories: categoriesSlice,
+  errors: errorsSlice,
+  toolkitModal: sliceModal,
+  toolkitForm: sliceForm,
+  product: productSlice,
+  filters: filtersSlice,
+  basket: basketSlice,
 });
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['basket'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
 export default store;
