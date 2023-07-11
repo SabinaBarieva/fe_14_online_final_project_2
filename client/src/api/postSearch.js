0e85588861afb1537a3cef587234df929d1c6265
Eugene Emets <eu.emets@gmail.com>
2023-07-11 12:51:49 +0300

    remove console.log

--- client/src/api/postSearch.js
+++ client/src/api/postSearch.js
@@ -3,7 +3,6 @@ import fetchApi from './fetchApi';
 
 const postSearch = async (searchQuery) => {
   if (!searchQuery) return [];
-  console.log('sq', searchQuery);
   return fetchApi(searchEP, {
     method: 'POST',
     body: JSON.stringify({ query: searchQuery }),
