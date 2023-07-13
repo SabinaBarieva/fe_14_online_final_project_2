c2261f58f3d981f9362c2087979f8e6b57a1fa33
Vadim Zayats <vadyk.za@gmail.com>
2023-07-13 18:23:00 +0300

    superfluous lines prom api/Getproduct delteted

--- client/src/api/getProduct.js
+++ client/src/api/getProduct.js
@@ -1,7 +1,6 @@
-import { currentProductEP, productsEP } from './constants';
+import { currentProductEP } from './constants';
 import fetchApi from './fetchApi';
 
 const getProduct = async (itemNo) => fetchApi(currentProductEP(itemNo));
-export const getProducts = async () => fetchApi(productsEP());
 
 export default getProduct;
