15687bfe830c9a7450531c07ade434ca975503e5
Vadim Zayats <vadyk.za@gmail.com>
2023-07-13 18:30:07 +0300

    Roboto font added to themeMain

--- client/src/components/ProductDescription/index.jsx
+++ client/src/components/ProductDescription/index.jsx
@@ -216,6 +216,7 @@ function ProductDescription() {
                 <Button
                   disabled={quantity === 0}
                   sx={{
+                    fontFamily: 'Roboto',
                     marginTop: { xs: '10px', md: '0' },
                     padding: '9px 18px',
                     backgroundColor: { xs: '#F5F7FB', md: '#211F1C' },
