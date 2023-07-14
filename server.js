ca1b13b063a8948e43a5b67ee73760cf3b6cfd45
Eugene Emets <eu.emets@gmail.com>
2023-07-14 21:07:28 +0300

    add some modifications to server.js, now its code in 2 separate files (needed by vercel)

--- server.js
+++ server.js
@@ -67,18 +67,21 @@ app.use('/api/comments', comments);
 app.use('/api/shipping-methods', shippingMethods);
 app.use('/api/payment-methods', paymentMethods);
 app.use('/api/partners', partners);
+app.use('/api/novaposhta', novaposhta);
 // app.use('/', mainRoute);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'static')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendStatus(404);
   });
 }
+module.exports = app;
+//const port = process.env.PORT || 4000;
 
-const port = process.env.PORT || 4000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+//app.listen(port, () => console.log(`Server running on port ${port}`));
