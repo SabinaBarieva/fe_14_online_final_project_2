65cd7f75abe39c681f7ea20e8bf7fa98f12573c5
Vadim Zayats <vadyk.za@gmail.com>
2023-07-15 14:54:39 +0300

    the lowest box with map and contacts not almost ready

---
+++ client/src/themes/themeAboutUs.js
@@ -0,0 +1,10 @@
+import { styled } from '@mui/system';
+
+export const Title = styled('h2')({
+  margin: '0',
+  fontWeight: '400',
+  fontSize: '40px',
+  lineHeight: '47px',
+  color: '#616467',
+  textAlign: 'center',
+});
