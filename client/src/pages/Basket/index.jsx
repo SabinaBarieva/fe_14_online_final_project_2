696954269ccc280bfb880202d90bbe44d33a329b
Sabina Bariyeva <badalova.s@gmail.com>
2023-07-17 14:25:04 +0300

    Edit style for basket

--- client/src/pages/Basket/index.jsx
+++ client/src/pages/Basket/index.jsx
@@ -25,6 +25,7 @@ import ButtonOrder from '../../components/Order/ButtonOrder';
 import { selectCart } from '../../redux/selectors';
 import {
   addToBasket,
+  clearBasket,
   deleteBasket,
   minusItem,
 } from '../../redux/slices/basketSlice';
@@ -34,11 +35,11 @@ import BasketEmpty from '../../components/BasketEmpty';
 import ModalBasket from '../../components/ModalForBasket';
 
 const columns = [
-  { id: 'product', label: 'Product' },
-  { id: 'name', label: '' },
-  { id: 'price', label: 'Price' },
-  { id: 'quantity', label: 'Quantity' },
-  { id: 'total', label: 'Total' },
+  { id: 'product', label: 'Product', width: '25%' },
+  { id: 'name', label: '', width: '30%' },
+  { id: 'price', label: 'Price', width: '15%' },
+  { id: 'quantity', label: 'Quantity', width: '15%' },
+  { id: 'total', label: 'Total', width: '15%' },
 ];
 
 const Title = styled('div')({
@@ -56,28 +57,44 @@ function BasketContent() {
   const { priceAll, itemsBasket } = useSelector(selectCart);
   /*   const totalCount = itemsBasket.reduce((sum, item) => sum + item.count, 0); */
   const dispatch = useDispatch();
-
+  const onClickClear = () => {
+    if (window.confirm('Очистить корзину?')) {
+      dispatch(clearBasket());
+    }
+  };
   if (!priceAll) {
     return <BasketEmpty />;
   }
   return (
-    <Container maxWidth="xxl">
+    <Grid>
       <ModalBasket />
       <Table
         key={Math.random()}
-        style={{
-          padding: '2rem',
+        sx={{
+          margin: {
+            xs: '0',
+            sm: '1',
+            md: '2rem',
+            lg: '3.5rem',
+          },
         }}>
         <TableHead key={Math.random()}>
           <TableRow key={Math.random()}>
             {columns.map((column) => (
               <TableCell
-                style={{
+                sx={{
                   fontFamily: theme.typography.fontFamily.primary,
                   color: '#000000',
                   fontWeight: '500',
-                  fontSize: '1rem',
                   borderBottom: '0',
+                  padding: {
+                    xs: '0.5rem',
+                    sm: '1rem',
+                    md: '1.5rem',
+                    lg: '2rem',
+                  },
+                  width: column.width,
+                  fontSize: { xs: '1rem', sm: '1.2rem', md: '1.5rem' },
                 }}
                 key={column.id}>
                 {column.label}
@@ -90,44 +107,69 @@ function BasketContent() {
             <TableRow key={Math.random()} style={{ marginBottom: '1 rem' }}>
               <TableCell
                 key={Math.random()}
-                style={{
+                sx={{
                   fontFamily: theme.typography.fontFamily.primary,
                   fontWeight: '600',
                   color: '#000000',
-                  padding: '10px',
+                  padding: {
+                    xs: '0.5rem',
+                    md: '1.5rem 1.5rem 1.5rem 0',
+                  },
                 }}>
-                <AdvancedImage
-                  key={Math.random()}
-                  cldImg={cld
-                    .image(el.imageUrls[0])
-                    .resize(
-                      pad().width(80).height(100).background(clColor('white'))
-                    )}
-                />
+                <Tooltip title="Detail">
+                  <Link
+                    style={{ textDecoration: 'none', color: '#000000' }}
+                    to={`/product/${el.itemNo}`}>
+                    <AdvancedImage
+                      key={Math.random()}
+                      cldImg={cld.image(el.imageUrls[0])}
+                      width="65%"
+                      height="60%"
+                    />
+                  </Link>
+                </Tooltip>
               </TableCell>
               <TableCell
                 key={Math.random()}
-                style={{
+                sx={{
                   fontFamily: theme.typography.fontFamily.primary,
                   fontWeight: '500',
-                  color: '#000000',
-                  padding: '5px',
+                  padding: {
+                    xs: '0.5rem',
+                    md: '1.5rem',
+                  },
+                  fontSize: { xs: '0.7rem', sm: '1.2rem', md: '1.5rem' },
                 }}>
-                {el.name}
+                <Tooltip title="Detail">
+                  <Link
+                    style={{ textDecoration: 'none', color: '#000000' }}
+                    to={`/product/${el.itemNo}`}>
+                    {el.name}
+                  </Link>
+                </Tooltip>
               </TableCell>
               <TableCell
                 key={Math.random()}
-                style={{
+                sx={{
                   fontFamily: theme.typography.fontFamily.primary,
-                  padding: '10px',
+                  fontSize: { xs: '0.7rem', sm: '1.2rem', md: '1.5rem' },
+                  padding: {
+                    xs: '0.5rem',
+                    md: '1.5rem',
+                  },
                 }}>
                 $ {el.currentPrice * el.count}
               </TableCell>
               <TableCell
                 key={Math.random()}
-                style={{
+                sx={{
                   fontFamily: theme.typography.fontFamily.primary,
-                  padding: '5px',
+                  padding: {
+                    xs: '0.2rem',
+                    sm: '1rem',
+                    md: '2rem',
+                  },
+                  fontSize: { xs: '0.7rem', sm: '1.2rem', md: '1.5rem' },
                 }}>
                 <IconButton
                   disabled={el.count === 1}
@@ -140,15 +182,31 @@ function BasketContent() {
                   <BiPlus fontSize="15" />
                 </IconButton>
               </TableCell>
-              <TableCell key={Math.random()}>
+              <TableCell
+                key={Math.random()}
+                sx={{
+                  padding: {
+                    xs: '0.7rem',
+                    sm: '1rem',
+                    md: '1.5rem',
+                  },
+                }}>
                 <Tooltip title="Delete">
-                  <IconButton key={Math.random()} aria-label="delete">
-                    <LiaTrashSolid
-                      fontSize="15"
-                      onClick={() =>
-                        dispatch(deleteBasket({ itemNo: el.itemNo }))
-                      }
-                    />
+                  <IconButton
+                    onClick={() =>
+                      dispatch(deleteBasket({ itemNo: el.itemNo }))
+                    }
+                    sx={{
+                      fontSize: {
+                        xs: '15px',
+                        sm: '30px',
+                        md: '40px',
+                        lg: '50px',
+                      },
+                    }}
+                    key={Math.random()}
+                    aria-label="delete">
+                    <LiaTrashSolid />
                   </IconButton>
                 </Tooltip>
               </TableCell>
@@ -220,7 +278,8 @@ function BasketContent() {
           </Grid>
         </Grid>
       </Grid>
-    </Container>
+      <Button onClick={onClickClear}>clear</Button>
+    </Grid>
   );
 }
 
