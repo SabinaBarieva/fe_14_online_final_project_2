a34fdbe5cd6c3b8a9e3c3679b577978415f496e4
Eugene Emets <eu.emets@gmail.com>
2023-07-17 14:02:59 +0300

    Carousel fix (#88)
    * created component Carousel
    
    * working from Carousel
    
    * correction files
    
    * correction files after  merge
    
    * save files
    
    * change swiper
    
    * change swiper, added products
    
    * added new carousel (material ui)
    
    * created new component carousel
    
    * new carousel
    
    * delete file
    
    * styled swiper
    
    * changed styles, added sales of goods
    
    * added swiper autoplay
    
    * deleted console
    
    * added padding swiper
    
    * added loading, changed styles
    
    * resolve conflict
    
    * fix problems with build
    
    * add --legacy-peer-deps for install command
    
    * fix styles
    
    * fix styles again
    
    ---------
    
    Co-authored-by: Kostiantyn Hrytsai <kos2@ukr.net>

---
+++ client/src/components/Carousel/index.jsx
@@ -0,0 +1,70 @@
+/* eslint-disable import/no-unresolved */
+import React, { useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+import { useSelector, useDispatch } from 'react-redux';
+import { LinearProgress } from '@mui/material';
+import { useTheme } from '@mui/system';
+import { getAllProducts } from '../../redux/slices/allProdsSlice';
+import ProductCard from '../ProductCard';
+import 'swiper/scss';
+import 'swiper/scss/autoplay';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import './styles.scss';
+
+function Carousel() {
+  const theme = useTheme();
+  const products = useSelector((state) => state.allProducts.allProds);
+  const isFetching = useSelector((state) => state.allProducts.isFetching);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllProducts({}));
+  }, [dispatch]);
+  return (
+    <div>
+      {isFetching ? (
+        <LinearProgress
+          sx={{
+            backgroundColor: `${theme.palette.primary.light}`,
+            width: '80%',
+            mx: 'auto',
+          }}
+        />
+      ) : (
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          autoplay={{ delay: 5000 }}
+          loop
+          pagination={{
+            clickable: true,
+          }}
+          navigation
+          modules={[Pagination, Navigation, Autoplay]}
+          className="saleCarousel"
+          breakpoints={{
+            440: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+            980: {
+              slidesPerView: 4,
+            },
+          }}>
+          {products
+            .filter((el) => el.sale === true)
+            .map((product) => (
+              <SwiperSlide>
+                <ProductCard product={product} />
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      )}
+    </div>
+  );
+}
+export default Carousel;
