b38aa71536faa0145a1eec90fec53e03696ccc3a
Vadim Zayats <vadyk.za@gmail.com>
2023-07-21 21:44:22 +0300

    product slice reverted

--- client/src/redux/slices/productSlice.js
+++ client/src/redux/slices/productSlice.js
@@ -1,10 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import getProductApi from '../../api/getProduct';
-import extraReducerCreator, {
-  initialStateCreator,
-} from './extraReducerCreator';
-
-const stateName = 'product';
 
 export const getProduct = createAsyncThunk(
   'product/getProduct',
@@ -13,17 +8,63 @@ export const getProduct = createAsyncThunk(
 
 export const productSlice = createSlice({
   name: 'product',
-  initialState: initialStateCreator(stateName),
+  initialState: {
+    product: {},
+    isLoading: null,
+    error: null,
+  },
   reducers: {
     setProduct: (state, action) => {
       state.product = action.payload;
     },
   },
   extraReducers: (builder) => {
-    extraReducerCreator(builder)(getProduct, stateName);
+    builder.addCase(getProduct.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(getProduct.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.product = action.payload;
+    });
+    builder.addCase(getProduct.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
   },
 });
 
 export const { setProduct } = productSlice.actions;
 
 export default productSlice.reducer;
+// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import getProductApi from '../../api/getProduct';
+// import extraReducerCreator, {
+//   initialStateCreator,
+// } from './extraReducerCreator';
+
+// const stateName = 'product';
+
+// export const getProduct = createAsyncThunk(
+//   'product/getProduct',
+//   async (itemNo) => getProductApi(itemNo)
+// );
+
+// export const productSlice = createSlice({
+//   name: 'product',
+//   initialState: initialStateCreator(stateName),
+//   reducers: {
+//     setProduct: (state, action) => {
+//       state.product = action.payload;
+//     },
+//     clearProduct: (state) => {
+//       state.product = null;
+//     },
+//   },
+//   extraReducers: (builder) => {
+//     extraReducerCreator(builder)(getProduct, stateName);
+//   },
+// });
+
+// export const { setProduct } = productSlice.actions;
+
+// export default productSlice.reducer;
