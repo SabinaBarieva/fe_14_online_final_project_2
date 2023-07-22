913a0bef8ac29a6b66eb4aa9c482f469959defb4
Vadim Zayats <vadyk.za@gmail.com>
2023-07-22 08:27:07 +0300

    product slice fixed

--- client/src/redux/slices/productSlice.js
+++ client/src/redux/slices/productSlice.js
@@ -30,36 +30,3 @@ export const productSlice = createSlice({
 export const { setProduct, clearProduct } = productSlice.actions;
 
 export default productSlice.reducer;
-
-// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// import getProductApi from '../../api/getProduct';
-// import extraReducerCreator, {
-//   initialStateCreator,
-// } from './extraReducerCreator';
-
-// const stateName = 'product';
-
-// export const getProduct = createAsyncThunk(
-//   'product/getProduct',
-//   async (itemNo) => getProductApi(itemNo)
-// );
-
-// export const productSlice = createSlice({
-//   name: 'product',
-//   initialState: initialStateCreator(stateName),
-//   reducers: {
-//     setProduct: (state, action) => {
-//       state.product = action.payload;
-//     },
-//     clearProduct: (state) => {
-//       state.product = null;
-//     },
-//   },
-//   extraReducers: (builder) => {
-//     extraReducerCreator(builder)(getProduct, stateName);
-//   },
-// });
-
-// export const { setProduct } = productSlice.actions;
-
-// export default productSlice.reducer;
