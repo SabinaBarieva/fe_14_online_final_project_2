0fdd85ed2d9877b8cc314d4346dd69722197fa48
Eugene Emets <eu.emets@gmail.com>
2023-07-25 15:52:05 +0300

    add useToken

---
+++ client/src/custom-hooks/useLocalStorage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const useLocalStorage = (storageKey, fallbackState) => {
+  const [value, setValue] = React.useState(
+    JSON.parse(localStorage.getItem(storageKey)) ?? fallbackState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(storageKey, JSON.stringify(value));
+  }, [value, storageKey]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;
