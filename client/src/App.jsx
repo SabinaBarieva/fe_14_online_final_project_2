13e5af7e6ceb48578c5bcea5a8ca8ee4ea5f8cb3
Eugene Emets <eu.emets@gmail.com>
2023-07-26 12:02:31 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,9 +15,9 @@ import theme from './themes/theme';
 import ProductsContent from './pages/Products';
 import About from './components/AboutUs';
 import UserPage from './pages/UserPage';
-import TestSignUp from './components/TestSignUp';
-import TestLogin from './components/TestLogin';
-import Init from './components/Init/Init';
+// import TestSignUp from './components/TestSignUp';
+// import TestLogin from './components/TestLogin';
+import Init from './components/Init';
 
 function App() {
   return (
@@ -42,7 +42,7 @@ function App() {
             </Routes>
           </BrowserRouter>
           {/* <TestSignUp /> */}
-          <TestLogin />
+          {/* <TestLogin /> */}
         </ThemeProvider>
       </PersistGate>
     </Provider>
