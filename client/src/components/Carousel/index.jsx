08595bd69c8be2eff7d9e51e37147a1160587f18
Vadym <59732160+vadym-zaiats@users.noreply.github.com>
2023-07-28 20:02:09 +0300

    carousel added (#167)

---
+++ client/src/components/Carousel/index.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable import/no-unresolved */
+import React, { useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { CircularProgress } from '@mui/material';
+import { AdvancedImage } from '@cloudinary/react';
+import { getAllSaleProducts } from '../../redux/slices/allProdsSlice';
+import getImg from '../../cloudinary';
+import {
+  allProductsInBase,
+  allProductsIsFetching,
+} from '../../redux/selectors';
+import 'swiper/scss';
+import 'swiper/scss/autoplay';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import './styles.scss';
+
+function Carousel() {
+  const products = useSelector(allProductsInBase);
+  const isFetching = useSelector(allProductsIsFetching);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllSaleProducts());
+  }, [dispatch]);
+  return (
+    <div>
+      {isFetching ? (
+        <CircularProgress color="success" style={{ marginLeft: '50%' }} />
+      ) : (
+        <Swiper
+          slidesPerView={1}
+          autoplay={{ delay: 5000 }}
+          loop
+          pagination={{
+            clickable: true,
+          }}
+          navigation
+          modules={[Pagination, Navigation, Autoplay]}
+          className="saleCarousel">
+          {products.map((product) => (
+            <SwiperSlide key={product.itemNo}>
+              <Link
+                to={`/product/${product.itemNo}`}
+                style={{ marginRight: '7%' }}>
+                <AdvancedImage
+                  className="main-photo"
+                  width="100%"
+                  cldImg={getImg.image(product.saleImg)}
+                  alt="our-photo"
+                />
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
+    </div>
+  );
+}
+export default Carousel;
