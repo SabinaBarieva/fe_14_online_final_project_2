bd73c0c1a16af7100a043bd19e2152e441176c90
Eugene Emets <eu.emets@gmail.com>
2023-07-30 22:38:10 +0300

    draft of toggleWishlist function

--- client/src/redux/slices/wishlistSlice/toggleProductInWishlist.js
+++ client/src/redux/slices/wishlistSlice/toggleProductInWishlist.js
@@ -1,16 +1,21 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { getToken } from '../../../localstorage/localstorage';
 import { updateWishList } from '../../../api/wishlist';
+import clone from '../../../helpers/clone';
 
 const toggleProductInWishlist = createAsyncThunk(
   'wishlost/toggle-product',
   async ({ productId }, { getState }) => {
+    console.log(productId);
     const isLoggedIn = getToken() && true;
-    let { wishlist } = JSON.parce(JSON.stringify(getState().wishlist));
+    const a = getState().wishlist;
+    let { wishlist } = clone(a);
     const isProductInWishList = wishlist.includes(productId);
-    wishlist = isProductInWishList
-      ? wishlist.filter((wishlistProductId) => wishlistProductId !== productId)
-      : wishlist.push(productId);
+    if (isProductInWishList)
+      wishlist = wishlist.filter(
+        (wishlistProductId) => wishlistProductId !== productId
+      );
+    else wishlist.push(productId);
     if (isLoggedIn) {
       const { product: remoteWishList } = await updateWishList(wishlist);
       return remoteWishList;
