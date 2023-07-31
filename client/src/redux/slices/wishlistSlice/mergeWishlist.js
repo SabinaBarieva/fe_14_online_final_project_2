e7cd3ba75d5d1e9e2f599898d6b80bdd28744e7c
Eugene Emets <eu.emets@gmail.com>
2023-07-31 11:24:22 +0300

    fix mergeWishList

--- client/src/redux/slices/wishlistSlice/mergeWishlist.js
+++ client/src/redux/slices/wishlistSlice/mergeWishlist.js
@@ -2,16 +2,23 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import clone from '../../../helpers/clone';
 import { getToken } from '../../../localstorage/localstorage';
 import { getWishList, updateWishList } from '../../../api/wishlist';
+import { isLoggedIn } from '../../selectors';
+import isProductInWishlist from './isProductInWishlist';
 
 const mergeWishlist = createAsyncThunk(
   'wishlist/merge',
   async (_, { getState }) => {
-    const wishlist = clone(getState.wishlist.wishlist);
-    const { products: remoteWishlist } = await getWishList();
-    const mergedWishlist = Array.from(
-      new Set([...wishlist, ...remoteWishlist])
-    );
-    const { products: resultWishList } = updateWishList(mergedWishlist);
+    const wishlist = clone(getState().wishlist.wishlist);
+    let remoteWishList = [];
+    if (isLoggedIn) {
+      const { products: remoteWishlistProducts } = await getWishList();
+      remoteWishList = remoteWishlistProducts;
+    }
+    wishlist.forEach((product) => {
+      if (!isProductInWishlist(remoteWishList, product))
+        remoteWishList.push(product);
+    });
+    const { products: resultWishList } = await updateWishList(remoteWishList);
     return resultWishList;
   }
 );
