62bf2d485fd34a0366216a3044157be4d4ffb29b
Eugene Emets <eu.emets@gmail.com>
2023-08-02 10:03:03 +0300

    add catching

--- client/src/api/getProduct.js
+++ client/src/api/getProduct.js
@@ -1,6 +1,25 @@
+import {
+  AppError,
+  notFoundErrorMessage,
+  productNotFoundErrorMessage,
+} from '../errors/errors';
 import { currentProductEP } from './constants';
 import fetchApi from './fetchApi';
 
-const getProduct = async (itemNo) => fetchApi(currentProductEP(itemNo));
-
+const getProduct = async (itemNo) => {
+  try {
+    const product = await fetchApi(currentProductEP(itemNo));
+    return product;
+  } catch (error) {
+    if (
+      error.context.status === 400 &&
+      /Product with itemNo .* is not found/.test(error.context.message)
+    ) {
+      throw new AppError(productNotFoundErrorMessage, {
+        context: { ...error.context.context },
+      });
+    }
+    throw error;
+  }
+};
 export default getProduct;
