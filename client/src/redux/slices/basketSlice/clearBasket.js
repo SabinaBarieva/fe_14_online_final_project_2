e6a61ea71d7ee4f0e6c128522faa51a9bdbcc927
Eugene Emets <eu.emets@gmail.com>
2023-08-03 09:12:32 +0300

    fix

--- client/src/redux/slices/basketSlice/clearBasket.js
+++ client/src/redux/slices/basketSlice/clearBasket.js
@@ -3,13 +3,15 @@ import { getToken } from '../../../localstorage/localstorage';
 import { deleteCart } from '../../../api/cart';
 import { handleAppError2 } from '../../../errors/errors';
 
-const clearBasket = createAsyncThunk('clear-basket', (_, { dispatch }) => {
-  const isLoggedIn = getToken() && true;
-  if (isLoggedIn) {
-    handleAppError2(dispatch)(() => deleteCart());
-    // await deleteCart();
+const clearBasket = createAsyncThunk(
+  'clear-basket',
+  async (_, { dispatch }) => {
+    const isLoggedIn = getToken() && true;
+    if (isLoggedIn) {
+      await handleAppError2(dispatch)(() => deleteCart());
+    }
+    return [];
   }
-  return [];
-});
+);
 
 export default clearBasket;
