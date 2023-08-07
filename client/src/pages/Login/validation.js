140e97c47795a0d794f857570a72280948b391d8
Olena <76615036+Aeternus2020@users.noreply.github.com>
2023-08-07 04:54:59 +0200

    Tests (#199)
    * add tests
    
    * reverse selector
    
    * add test from order, search

--- client/src/pages/Login/validation.js
+++ client/src/pages/Login/validation.js
@@ -15,11 +15,6 @@ const validationSchema = Yup.object({
   password: Yup.string()
     .min(7, 'Min 7 letters required')
     .max(30, 'Max 30 letters allowed')
-    .matches(/^[a-zA-Z]/, 'Password must contain Latin letters only')
-    .matches(/\d/, 'Password must contain at least one number')
-    .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
-    .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
-    .matches(/^[a-zA-Z0-9]+$/, 'Password must not contain spaces')
     .required('This field is required!'),
 });
 
